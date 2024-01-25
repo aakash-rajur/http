d3209b5070941163ef48632d19c2fc9397546412
Aakash Rajur <aakashrajur16@gmail.com>
2024-01-25 22:56:31 +0530

    update test cases

--- router_test.go
+++ router_test.go
@@ -2,11 +2,15 @@ package http
 
 import (
 	"bytes"
+	"context"
+	"encoding/json"
 	"fmt"
+	"github.com/aakash-rajur/http/params"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
 func TestNewRouter(t *testing.T) {
@@ -665,7 +669,7 @@ func TestRouter_ServeHTTP(t *testing.T) {
 			}
 
 			for _, arg := range test.args {
-				body := bytes.NewReader([]byte("some important body"))
+				body := bytes.NewReader([]byte("some important Body"))
 
 				req := httptest.NewRequest(arg.method, arg.pattern, body)
 
@@ -675,7 +679,7 @@ func TestRouter_ServeHTTP(t *testing.T) {
 
 				assert.Equalf(t, http.StatusOK, rr.Code, "code should be %d", http.StatusOK)
 
-				assert.Equalf(t, arg.expectation, rr.Body.String(), "body should be %s", arg.expectation)
+				assert.Equalf(t, arg.expectation, rr.Body.String(), "Body should be %s", arg.expectation)
 
 				arg.mockHandler.AssertNumberOfCalls(t, "ServeHTTP", 1)
 			}
@@ -684,5 +688,277 @@ func TestRouter_ServeHTTP(t *testing.T) {
 				mockMiddleware.AssertNumberOfCalls(t, "Run", len(test.args))
 			}
 		})
+	}
+}
+
+func TestRouter_ServeHTTP_ParseApi(t *testing.T) {
+	tests := []TestRoute{
+		// Objects
+		{
+			Name:    "POST /1/classes/{className}",
+			Method:  "POST",
+			Pattern: "/1/classes/{className}",
+		},
+		{
+			Name:    "GET /1/classes/{className}/{objectId}",
+			Method:  "GET",
+			Pattern: "/1/classes/{className}/{objectId}",
+		},
+		{
+			Name:    "PUT /1/classes/{className}/{objectId}",
+			Method:  "PUT",
+			Pattern: "/1/classes/{className}/{objectId}",
+		},
+		{
+			Name:    "GET /1/classes/{className}",
+			Method:  "GET",
+			Pattern: "/1/classes/{className}",
+		},
+		{
+			Name:    "DELETE /1/classes/{className}/{objectId}",
+			Method:  "DELETE",
+			Pattern: "/1/classes/{className}/{objectId}",
+		},
+
+		// Users
+		{
+			Name:    "POST /1/users",
+			Method:  "POST",
+			Pattern: "/1/users",
+		},
+		{
+			Name:    "GET /1/login",
+			Method:  "GET",
+			Pattern: "/1/login",
+		},
+		{
+			Name:    "GET /1/users/{objectId}",
+			Method:  "GET",
+			Pattern: "/1/users/{objectId}",
+		},
+		{
+			Name:    "PUT /1/users/{objectId}",
+			Method:  "PUT",
+			Pattern: "/1/users/{objectId}",
+		},
+		{
+			Name:    "GET /1/users",
+			Method:  "GET",
+			Pattern: "/1/users",
+		},
+		{
+			Name:    "DELETE /1/users/{objectId}",
+			Method:  "DELETE",
+			Pattern: "/1/users/{objectId}",
+		},
+		{
+			Name:    "POST /1/requestPasswordReset",
+			Method:  "POST",
+			Pattern: "/1/requestPasswordReset",
+		},
+
+		// Roles
+		{
+			Name:    "POST /1/roles",
+			Method:  "POST",
+			Pattern: "/1/roles",
+		},
+		{
+			Name:    "GET /1/roles/{objectId}",
+			Method:  "GET",
+			Pattern: "/1/roles/{objectId}",
+		},
+		{
+			Name:    "PUT /1/roles/{objectId}",
+			Method:  "PUT",
+			Pattern: "/1/roles/{objectId}",
+		},
+		{
+			Name:    "GET /1/roles",
+			Method:  "GET",
+			Pattern: "/1/roles",
+		},
+		{
+			Name:    "DELETE /1/roles/{objectId}",
+			Method:  "DELETE",
+			Pattern: "/1/roles/{objectId}",
+		},
+
+		// Files
+		{
+			Name:    "POST /1/files/{fileName}",
+			Method:  "POST",
+			Pattern: "/1/files/{fileName}",
+		},
+
+		// Analytics
+		{
+			Name:    "POST /1/events/{eventName}",
+			Method:  "POST",
+			Pattern: "/1/events/{eventName}",
+		},
+
+		// Push Notifications
+		{
+			Name:    "POST /1/push",
+			Method:  "POST",
+			Pattern: "/1/push",
+		},
+
+		// Installations
+		{
+			Name:    "POST /1/installations",
+			Method:  "POST",
+			Pattern: "/1/installations",
+		},
+		{
+			Name:    "GET /1/installations/{objectId}",
+			Method:  "GET",
+			Pattern: "/1/installations/{objectId}",
+		},
+		{
+			Name:    "PUT /1/installations/{objectId}",
+			Method:  "PUT",
+			Pattern: "/1/installations/{objectId}",
+		},
+		{
+			Name:    "GET /1/installations",
+			Method:  "GET",
+			Pattern: "/1/installations",
+		},
+		{
+			Name:    "DELETE /1/installations/{objectId}",
+			Method:  "DELETE",
+			Pattern: "/1/installations/{objectId}",
+		},
+
+		// Cloud Functions
+		{
+			Name:    "POST /1/functions",
+			Method:  "POST",
+			Pattern: "/1/functions",
+		},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+
+		router := setupRouter(tests)
+
+		seed := fmt.Sprintf("parse:%d", time.Now().UnixNano())
+
+		t.Run(tc.Name, validate(tc, seed, router))
+	}
+}
+
+func setupRouter(testRoutes []TestRoute) http.Handler {
+	router := NewRouter()
+
+	m1 := func(w http.ResponseWriter, r *http.Request, next Next) {
+		ctx := context.WithValue(r.Context(), "m1", "m1")
+
+		rc := r.WithContext(ctx)
+
+		next(rc)
+	}
+
+	m2 := func(w http.ResponseWriter, r *http.Request, next Next) {
+		ctx := context.WithValue(r.Context(), "m2", "m2")
+
+		rc := r.WithContext(ctx)
+
+		next(rc)
+	}
+
+	router.Use(m1)
+
+	router.Use(m2)
+
+	handler := func(w http.ResponseWriter, r *http.Request) {
+		pathParams, ok := params.FromRequest(r)
+
+		if !ok {
+			pathParams = make(map[string]string)
+		}
+
+		body := make(map[string]any)
+
+		err := json.NewDecoder(r.Body).Decode(&body)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+
+			return
+		}
+
+		m1 := r.Context().Value("m1")
+
+		m2 := r.Context().Value("m2")
+
+		payload := map[string]interface{}{
+			"Params": pathParams,
+			"Path":   r.URL.Path,
+			"query":  r.URL.Query(),
+			"Method": r.Method,
+			"body":   body,
+			"m1":     m1,
+			"m2":     m2,
+		}
+
+		jsonPayload, err := json.Marshal(payload)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+
+		w.WriteHeader(http.StatusOK)
+
+		_, _ = w.Write(jsonPayload)
+	}
+
+	for _, tc := range testRoutes {
+		router.HandleMethodFunc(tc.Method, tc.Pattern, handler)
+	}
+
+	return router
+}
+
+func validate(tc TestRoute, seed string, router http.Handler) func(t *testing.T) {
+	return func(t *testing.T) {
+		tcp := tc.generateParams(seed)
+
+		want := map[string]any{
+			"m1":     "m1",
+			"m2":     "m2",
+			"Method": tcp.Method,
+			"Params": tcp.Params,
+			"Path":   tcp.Path,
+			"query":  map[string]any{},
+			"body":   tcp.Body.Map(),
+		}
+
+		r := httptest.NewRequest(tcp.Method, tcp.Path, tcp.Body.Reader())
+
+		w := httptest.NewRecorder()
+
+		router.ServeHTTP(w, r)
+
+		assert.Equalf(t, http.StatusOK, w.Code, "%s:%s code should be %d", tcp.Method, tcp.Pattern, http.StatusOK)
+
+		if w.Code != http.StatusOK {
+			return
+		}
+
+		got := make(map[string]any)
+
+		err := json.Unmarshal(w.Body.Bytes(), &got)
+
+		assert.NoErrorf(t, err, "should not error while unmarshalling Body")
+
+		assert.Equalf(t, want, got, "%s:%s Body should be %v", tcp.Method, tcp.Pattern, want)
 	}
 }
